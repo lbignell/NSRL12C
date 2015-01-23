a6c7b1d1682afd8fef4a6339a978855ede4c6805
Lindsey Bignell <lbignell@bnl.gov>
2015-01-23 10:07:53 -0500

    Modified some scripts to reflect recent changes in code.

--- NSRL12C-build/G4SetManualLY.sh
+++ NSRL12C-build/G4SetManualLY.sh
@@ -5,75 +5,77 @@ cp RunManualLY.mac RunSimple.mac
 ################
 #First run
 #./NSRL12C RunSimple.mac > log1.txt
-#echo Finished first run; move Edep.root if required...
-#mv Edep.root Water_2000MeV.root
+#echo Finished first run; move Edep_.root if required...
+#mv Edep_.root Water_2000MeV.root
 ################
 #Second run
 #perl -pi -e 's/2000/475/' RunSimple.mac
 #./NSRL12C RunSimple.mac > log2.txt
 #echo Finished second run!
-#mv Edep.root Water_475MeV.root
+#mv Edep_.root Water_475MeV.root
 ################
 #Third run
 #perl -pi -e 's/475/210/' RunSimple.mac
 ##./NSRL12C RunSimple.mac > log3.txt
 #echo Finished Third run!
-#mv Edep.root Water_210MeV.root
+#mv Edep_.root Water_210MeV.root
 ################
 #Fourth run
 #perl -pi -e 's/210/2000/' RunSimple.mac
 #Change from water to WbLS
 #perl -pi -e 's/true/false/' RunSimple.mac
-./NSRL12C RunSimple.mac > log4.txt
-echo Finished Fourth run!
-mv Edep.root WbLS_1pc_2000MeV.root
+#./NSRL12C RunSimple.mac > log4.txt
+#echo Finished Fourth run!
+#mv Edep_.root WbLS_1pc_2000MeV.root
 ################
 #Fifth run
 perl -pi -e 's/2000/475/' RunSimple.mac
 ./NSRL12C RunSimple.mac > log5.txt
 echo Finished Fifth run!
-mv Edep.root WbLS_1pc_475MeV.root
+#mv Edep_.root WbLS_1pc_475MeV.root
 ################
 #Sixth run
 perl -pi -e 's/475/210/' RunSimple.mac
 ./NSRL12C RunSimple.mac > log6.txt
 echo Finished Sixth run!
-mv Edep.root WbLS_1pc_210MeV.root
+#mv Edep_.root WbLS_1pc_210MeV.root
 ################
 #Seventh run
+perl -pi -e 's/_1pc_/_0p4pc_/' RunSimple.mac
 perl -pi -e 's/210/2000/' RunSimple.mac
 perl -pi -e 's/0.0099/0.004/' RunSimple.mac
 perl -pi -e 's/105/15/' RunSimple.mac
 perl -pi -e 's/0.329/0.15/' RunSimple.mac
 ./NSRL12C RunSimple.mac > log7.txt
 echo Finished Seventh run!
-mv Edep.root WbLS_0p4pc_2000MeV.root
+#mv Edep_.root WbLS_0p4pc_2000MeV.root
 ################
 #Eighth run
 perl -pi -e 's/2000/475/' RunSimple.mac
 ./NSRL12C RunSimple.mac > log8.txt
 echo Finished Eighth run!
-mv Edep.root WbLS_0p4pc_475MeV.root
+#mv Edep_.root WbLS_0p4pc_475MeV.root
 ################
 #Ninth run
 perl -pi -e 's/475/210/' RunSimple.mac
 ./NSRL12C RunSimple.mac > log9.txt
 echo Finished Ninth run!
-mv Edep.root WbLS_0p4pc_210MeV.root
+#mv Edep_.root WbLS_0p4pc_210MeV.root
 #I can add more runs to do LS simulations once I get the data...
 ################
 #PureLS samples
+#perl -pi -e 's/_0p4pc_/_PureLS_/' RunSimple.mac
 #perl -pi -e 's/210/2000/' RunSimple.mac
 #perl -pi -e 's/0.004/1./' RunSimple.mac
 #perl -pi -e 's/15/8400/' RunSimple.mac
-#perl -pi -e 's/20000/3000/' RunSimple.mac
+#perl -pi -e 's/25000/3000/' RunSimple.mac
 #./NSRL12C RunSimple.mac > log10.txt
-#mv Edep.root PureLS_2000MeV.root
+#mv Edep_.root PureLS_2000MeV.root
 #475 MeV protons
 #perl -pi -e 's/2000/475/' RunSimple.mac
 #./NSRL12C RunSimple.mac > log11.txt
-#mv Edep.root PureLS_475MeV.root
+#mv Edep_.root PureLS_475MeV.root
 #210 MeV Protons
 #perl -pi -e 's/475/210/' RunSimple.mac
 #./NSRL12C RunSimple.mac > log12.txt
-#mv Edep.root PureLS_210MeV.root
+#mv Edep_.root PureLS_210MeV.root

c460b52cf47dc9b1a4dfb2cfe9d411662f86dda5
Lindsey Bignell <lbignell@bnl.gov>
2015-01-23 16:20:34 -0500

    Stopped tracking new.txt, modified G4BirksRun.sh, and README

--- NSRL12C-build/G4BirksRun.sh
+++ NSRL12C-build/G4BirksRun.sh
@@ -132,27 +132,35 @@ perl -pi -e 's/7000/3000/' RunSimple.mac
 #./NSRL12C RunSimple.mac
 #mv Edep.root WbLS_PureLS_kB0.1_210MeV.root
 ################
-perl -pi -e 's/0.1/0.15/' RunSimple.mac
+perl -pi -e 's/0.1/0.05/' RunSimple.mac
+./NSRL12C RunSimple.mac
+mv Edep_.root WbLS_PureLS_kB0.05_210MeV.root
+################
+perl -pi -e 's/0.05/0.0/' RunSimple.mac
+./NSRL12C RunSimple.mac
+mv Edep_.root WbLS_PureLS_kB0.0_210MeV.root
+################
+#perl -pi -e 's/0.1/0.15/' RunSimple.mac
 #./NSRL12C RunSimple.mac
 #mv Edep.root WbLS_PureLS_kB0.15_210MeV.root
 ################
-perl -pi -e 's/0.15/0.2/' RunSimple.mac
+#perl -pi -e 's/0.15/0.2/' RunSimple.mac
 #./NSRL12C RunSimple.mac
 #mv Edep.root WbLS_PureLS_kB0.2_210MeV.root
 ################
-perl -pi -e 's/0.2/0.25/' RunSimple.mac
-./NSRL12C RunSimple.mac
-mv Edep.root WbLS_PureLS_kB0.25_210MeV.root
+#perl -pi -e 's/0.2/0.25/' RunSimple.mac
+#./NSRL12C RunSimple.mac
+#mv Edep.root WbLS_PureLS_kB0.25_210MeV.root
 ################
-perl -pi -e 's/0.25/0.3/' RunSimple.mac
-./NSRL12C RunSimple.mac
-mv Edep.root WbLS_PureLS_kB0.3_210MeV.root
+#perl -pi -e 's/0.25/0.3/' RunSimple.mac
+#./NSRL12C RunSimple.mac
+#mv Edep.root WbLS_PureLS_kB0.3_210MeV.root
 ################
-perl -pi -e 's/0.3/0.35/' RunSimple.mac
-./NSRL12C RunSimple.mac
-mv Edep.root WbLS_PureLS_kB0.35_210MeV.root
+#perl -pi -e 's/0.3/0.35/' RunSimple.mac
+#./NSRL12C RunSimple.mac
+#mv Edep.root WbLS_PureLS_kB0.35_210MeV.root
 ################
-perl -pi -e 's/0.35/0.4/' RunSimple.mac
-./NSRL12C RunSimple.mac
-mv Edep.root WbLS_PureLS_kB0.4_210MeV.root
+#perl -pi -e 's/0.35/0.4/' RunSimple.mac
+#./NSRL12C RunSimple.mac
+#mv Edep.root WbLS_PureLS_kB0.4_210MeV.root
 #The End.#
